212de8583e2d01d88de76ac7a65032a701f5f45c
Sang Nguyễn Ngọc <smt.ngocsang@gmail.com>
2023-07-20 14:15:51 +0700

    final

---
+++ Models/Lichsu/AdminSetting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace pdc.Models.Lichsu
+{
+    public partial class AdminSetting
+    {
+        public int Id { get; set; }
+        public string Mode { get; set; } = null!;
+        public int Value { get; set; }
+    }
+}
